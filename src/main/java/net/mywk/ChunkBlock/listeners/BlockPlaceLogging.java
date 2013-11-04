5c8c2532a9f0a0ee2774b4385f04954a84313718
Mywk <mywk@msn.com>
2013-11-04 17:17:52 +0000

    Unneeded import

--- src/main/java/net/mywk/ChunkBlock/listeners/BlockPlaceLogging.java
+++ src/main/java/net/mywk/ChunkBlock/listeners/BlockPlaceLogging.java
@@ -1,6 +1,5 @@
 package net.mywk.ChunkBlock.listeners;
 
-import org.bouncycastle.jcajce.provider.symmetric.AES.CBC;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
